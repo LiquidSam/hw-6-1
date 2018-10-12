dfb429262bbd179b9015e3f925df0c5f966bdffb
liquidsamlol <liquidsamlol@gmail.com>
2018-10-12 13:44:53 +0300

    321

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
+            int[] data = new int[6];
+            int i = 0;
+            while (i < 6)
+            {
+                Console.WriteLine("Введите элемент массива");
+
+                data[i] = Int16.Parse(Console.ReadLine());
+                Console.WriteLine();
+                i++;
+            }
+
+            for (i = 0; i < 6; i = i + 1)
+                Console.WriteLine("Элемент[" + i + "]: " + data[i]);
+            Console.ReadLine();
         }
     }
 }
