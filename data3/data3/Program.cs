1dc9ee78f604fb19e66672890f5e8434ca248f15
liquidsamlol <liquidsamlol@gmail.com>
2018-10-12 14:32:31 +0300

    35

---
+++ data3/data3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace data3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[5] {5,8,4,7,3};
+            Array.Sort(data); 
+
+            for (int i = 0; i < data.Length; i++)               
+                Console.Write(" " + data[i]);
+            Console.ReadKey();
+        }
+    }
+}
