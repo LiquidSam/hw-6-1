85a61b506cffccb47274bde707f9994177791e93
liquidsamlol <liquidsamlol@gmail.com>
2018-10-12 15:32:55 +0300

    213

---
+++ string2/string2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace string2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter poem, split strings with ','");
+            string text = Console.ReadLine();
+
+            string newtext = text.Replace('о', 'а');
+            string newtext1 =newtext.Replace("л", "ль");
+            string newtext2 =newtext1.Replace("ть", "т");
+            
+
+            String[] poem = newtext2.Split(new char[] { ',' });
+
+            foreach (string s in poem)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
