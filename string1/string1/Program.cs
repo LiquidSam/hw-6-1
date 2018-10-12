5306a3d0bfd0bb5d10308ca558b938f3d7f79008
liquidsamlol <liquidsamlol@gmail.com>
2018-10-12 15:06:01 +0300

    31

---
+++ string1/string1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace string1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string");
+            string text = Console.ReadLine();
+            int c = text.Length;
+            if (c>13)
+            {
+                string a = "...";
+                string t1 = text.Remove(13);
+                string t3 = t1 + a;
+                Console.WriteLine(t3);
+            }
+            else
+            {
+                Console.WriteLine(text);
+            }
+            
+        }
+    }
+}
